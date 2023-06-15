fec8c53d08bf875f61c43c1becbfbabf0bfe9835
Deep <105926444+Dpppatel@users.noreply.github.com>
2023-06-15 10:18:30 -0500

    FINAL VERSION

--- simple-cipher.js
+++ simple-cipher.js
@@ -4,139 +4,56 @@
 //
 
 export class Cipher {
-  constructor(key = 'd') {
-    // todo: implement
-    this.key = key.toLowerCase;
+  constructor(key = 'dddddddddd') {
+    this._key = key.toLowerCase().repeat(5);
   }
 
   encode(rawData) {
-    // todo: implement
-    this.rawData = rawData;
+    const letterZval = 'z'.charCodeAt(0); // 122
     let encodedData = '';
-    let convertValue = convertKey(this.key);
-    for(let i = 0; i < this.rawData.length; i++){
-      let asciiVal = this.rawData.charAt(i);
-      if(!asciiVal+convertValue >= 122){     // make sure to remove a problem that might cause it to go out of the 97-122 range of asciiVal;
-        asciiVal += convertValue;
-      } else {
-        asciiVal = 97 + convertValue;
-      }
-      encodedData += String.fromCharCode(asciiVal);
+    
+    
+    for(let i = 0; i < rawData.length; i++){
+      let letter = rawData.charCodeAt(i);
+      const shiftKey = this._key.charCodeAt(i) - 97;
+
+      letter += shiftKey; //key
+      
+      if(letter > letterZval){
+        // Wrap
+        letter -= 26; // number of alphabets
+      } 
+
+      //append to create a string
+      encodedData += String.fromCharCode(letter);
     }
+
+    return encodedData;
   }
 
   decode(encriptedData) {
-    // todo: implement
-    this.encriptedData = encriptedData;
+    const letterAval = 'a'.charCodeAt(0);
     let decodedData = '';
-    let convertValue = convertKey(this.key);
-    for(let i = 0; i < this.rawData.length; i++){
-      let asciiVal = this.rawData.charAt(i);
-      if(!asciiVal-convertValue <= 97){           // make sure to remove a problem that might cause it to go out of the 97-122 range of asciiVal;
-        asciiVal -= convertValue;
-      } else {
-        asciiVal = 122 - convertValue;
+    
+    
+    for(let i = 0; i < encriptedData.length; i++){
+      let letter = encriptedData.charCodeAt(i);
+      const shiftKey = this._key.charCodeAt(i) - 97;
+
+      letter -= shiftKey;
+      
+      if(letter < letterAval){
+        letter += 26;
       }
-      decodedData += String.fromCharCode(asciiVal);
+
+      decodedData += String.fromCharCode(letter);
     }
+
+    return decodedData;
   }
 
   get key() {
     // todo: implement
-    return this.charKey;
+    return this._key;
   }
-}
-
-//ascii will add to the current char to encode and subtract from to decode
-
-
-
-
-//function to convert from char to number for further use
-function convertKey(character){
-  character = character.charAt(0);
-  let returnValue = null;
-  switch (character) {
-    case 'a':
-      returnValue = 0;
-      break;
-    case 'b':
-      returnValue = 1;
-      break;
-    case 'c':
-      returnValue = 2;
-      break;
-    case 'd':
-      returnValue = 3;
-      break;
-    case 'e':
-      returnValue = 4;
-      break;
-    case 'f':
-      returnValue = 5;
-      break;
-    case 'g':
-      returnValue = 6;
-      break;
-    case 'h':
-      returnValue = 7;
-      break;
-    case 'i':
-      returnValue = 8;
-      break;
-    case 'j':
-      returnValue = 9;
-      break;
-    case 'k':
-      returnValue = 10;
-      break;
-    case 'l':
-      returnValue = 11;
-      break;
-    case 'm':
-      returnValue = 12;
-      break;
-    case 'n':
-      returnValue = 13;
-      break;
-    case 'o':
-      returnValue = 14;
-      break;
-    case 'p':
-      returnValue = 15;
-      break;
-    case 'q':
-      returnValue = 16;
-      break;
-    case 'r':
-      returnValue = 17;
-      break;
-    case 's':
-      returnValue = 18;
-      break;
-    case 't':
-      returnValue = 19;
-      break;
-    case 'u':
-      returnValue = 20;
-      break;
-    case 'v':
-      returnValue = 21;
-      break;
-    case 'w':
-      returnValue = 22;
-      break;
-    case 'x':
-      returnValue = 23
-      break;
-    case 'y':
-      returnValue = 24;
-      break;
-    case 'z':
-      returnValue = 25;
-      break;
-    default:
-      return null;
-  }
-  return returnValue;
 }
